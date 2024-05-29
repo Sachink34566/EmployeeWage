24f6f912d303829fd0b7fa21918a987f2cbd6cb5
Sachink34566 <sachinkumar98504@gmail.com>
2024-05-29 19:06:19 +0530

    add employee presences

---
+++ EmployeeWage/Presence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Presence
+    {
+        public class Employee
+        {
+            public void printpresence()
+            {
+                int IS_FULL_TIME = 1;
+                Random random = new Random();
+
+                int empCheck = random.Next(0, 2);
+                if (empCheck == IS_FULL_TIME)
+                {
+                    Console.WriteLine("Employee is Preaent");
+                }
+                else
+                {
+                    Console.WriteLine("Employee is not Present");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.printpresence();
+
+        }
+    }
+}
